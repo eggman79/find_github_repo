1091db21ce940cb598d00d07f26b01e0d50d5f6a
eggman79 <eggman79@protonmail.com>
2021-11-03 15:30:04 +0100

    initial

---
+++ find_github_repo.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+import re
+import argparse
+import urllib.request
+from urllib.parse import quote
+from bs4 import BeautifulSoup
+
+def check_has_next_page(soup):
+    if not soup.find_all('a', {'class': 'next_page'}):
+        return False
+
+    return not soup.find_all('a', {'class': 'next_page disabled'})
+
+def find_subpage_repos(params):
+    repos = []
+    uri_params = list(map(lambda key: f'{quote(key)}={quote(params[key])}', params))
+    prefix_url = 'https://github.com'
+    url = urllib.parse.urljoin(prefix_url, f'search?{"&".join(uri_params)}')
+
+    with urllib.request.urlopen(url) as github_content:
+        soup = BeautifulSoup(github_content.read(), 'html.parser')
+
+        for repo_node in soup.find_all('a', attrs={'href': re.compile(r'^\/[^\/]+\/[^?^&^\/]+$')}):
+            href = repo_node.get('href')
+
+            if re.match(r'^\/(topics|features)\/', href):
+                continue
+
+            repo_url = urllib.parse.urljoin(prefix_url, href)
+            repos.append(repo_url)
+
+        return (check_has_next_page(soup), repos)
+
+def find_repos():
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('reponame', type=str, nargs=1)
+        parser.add_argument('lang', type=str, nargs='?')
+        parser.add_argument('-c', help='max subpages count', type=int, nargs='?', default=1)
+        args = parser.parse_args()
+
+        params = {'q': args.reponame[0], 'type': 'Repositories'}
+
+        if args.lang:
+            params['l'] = args.lang
+
+        page_number = 1
+
+        while True:
+            (has_next_page, repos) = find_subpage_repos(params)
+            print('\n'.join(repos))
+
+            if not has_next_page:
+                break
+
+            if args.c == page_number:
+                break
+
+            page_number += 1
+            params['p'] = str(page_number)
+
+    except (SystemExit, argparse.ArgumentError):
+        pass
+    except BaseException as ex:
+        print(ex)
+
+find_repos()
